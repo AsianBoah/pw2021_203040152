0b47991c2112d8985a89b1b71c7d6095cc45e0a3
[AsianBoah] <fadhlan1057@gmail.com>
2021-02-25 21:56:22 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,48 @@
+<?php 
+/*
+Fadhlan Maulana
+203040152
+https://github.com/AsianBoah/pw2021_203040152
+Pertemuan 3 - Feb 25th 2021
+Mempelajari struktur kendali PHP 
+/*
+?>
+<?php 
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// for( $i = 0; $i < 5; $i++ ) {
+// 	echo "Welcome Mr. Aziz! <br>";
+// }
+
+// while ( $i < 5 ) {
+// 	echo "Hello Mr. Maulana! <br>";
+// $i++;
+// }
+
+// $i = 10;
+// do {
+// 	echo "Hello Fadhlan! <br>";
+// $i++;
+// } while ( $i < 5 );
+
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 30;
+if( $x < 20 ) {
+	echo "true";
+} else if( $x == 20 ) {
+	echo "bingo!";
+} else {
+	echo "salah";
+}
+
+
+?>
