ab84cd963fca23379c24b4144601ac1261989c5b
[AsianBoah] <fadhlan1057@gmail.com>
2021-02-25 18:12:56 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,65 @@
+<?php 
+/*
+Fadhlan Maulana
+203040152
+https://github.com/AsianBoah/pw2021_203040152
+Pertemuan 2 - Feb 25th 2021
+Mempelajari sintaks dasar PHP 
+/*
+?>
+<?php
+// Pertemuan 2 - PHP Dasar
+// SINTAKS PHP
+
+// Standard Output
+// echo, print
+// print_r
+// var_dump
+
+// Penulisan Sintaks PHP
+// 1.PHP di dalam HTML
+// 2.HTML di dalam PHP
+
+// Variable dan tipe data
+// Variable
+// Tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "ASTROWORLD";
+// echo "Hello, this is $nama";
+// note = ("") > ('') because interpolasi
+
+// OPERATOR
+// Aritmathics
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concetation / concat
+// .
+// $nama_depan = "Fadhlan";
+// $nama_belakang = "Maulana";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// = += -+ *= /= %= .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Fadhlan";
+// $nama .= " ";
+// $nama .= "Maulana";
+// echo $nama;
+
+// Perbandingan
+// < > <= >= ==
+// var_dump(1 == "1");
+
+// Identity
+// === !==
+// var_dump(1 === "1");
+
+// Logic
+// && || !
+// $x - 10;
+// var_dump($x < 20 || $x % 2 == 0);
+?>
